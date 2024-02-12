433914a574a3a2ebc3267a2ffa1deb237af81fd8
ozioma45 <oziomaegole@gmail.com>
2024-02-12 17:16:58 +0100

    create new assertion method without using assertion library

--- sample.js
+++ sample.js
@@ -12,28 +12,34 @@ const { sum, substract } = require("./math");
 
 let result = sum(5, 5);
 let expected = 10;
+expect(result).toBe(expected);
 
-if (result != expected) {
+/* if (result != expected) {
   throw new Error(`${result} is not equal to ${expected}`);
 } else {
   console.log(`All test passed`);
-}
+} */
 
 result = substract(5, 4);
 expected = 1;
+expect(result).toBe(expected);
 
-if (result != expected) {
+/* if (result != expected) {
   throw new Error(`${result} is not equal to ${expected}`);
 } else {
   console.log(`All test passed`);
-}
+} */
 
 function expect(actual) {
   return {
     toBe(expected) {
       if (actual != expected) {
-        throw new E();
+        throw new Error(`${actual} is not equal to ${expected}`);
+      } else {
+        console.log("Test Passed");
       }
     },
+    toEqual(expected) {},
+    toBeGreaterThan(expected) {},
   };
 }
